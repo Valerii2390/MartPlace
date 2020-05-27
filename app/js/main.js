23cf257f2697017c2760317a49dc1c4267779f68
Valerii2390-dev <valezh14@yahoo.com>
2020-05-27 19:49:40 +0300

    added body

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
-$(function() {
+$(document).ready(function() {
+    $('.slider').slick({
+        prevArrow: '<button type="button" class="slick-arrow slick-arrow--prev"><span class="lnr lnr-chevron-left"></span></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-arrow--next"><span class="lnr lnr-chevron-right"></span></button>',
+        appendArrows: '.arrows-wrap'
+    })
+    $('.slider__items').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: '<button type="button" class="slick-arrow slick-arrow--prev"><span class="lnr lnr-chevron-left"></span></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-arrow--next"><span class="lnr lnr-chevron-right"></span></button>',
+        appendArrows: '.arrows-wrap'
 
+    })
+    $('.slider1').slick({
+
+        prevArrow: '<button type="button" class="slick-arrow slick-arrow--prev"><span class="lnr lnr-chevron-left"></span></button>',
+        nextArrow: '<button type="button" class="slick-arrow slick-arrow--next"><span class="lnr lnr-chevron-right"></span></button>',
+        appendArrows: '.arrows-wrap'
+
+    })
 });
